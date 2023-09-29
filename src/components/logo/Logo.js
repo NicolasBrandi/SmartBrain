4010144722263b3af372017b7436170dda4e29fc
Nicolas Brandi <nmoyanobrandi@gmail.com>
2023-09-29 12:02:14 -0700

    add logo and nav componets

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import Brain from "./brain.png"
+import "./Logo.css";
+
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0" >
+      <Tilt className="Tilt br2 shadow-3" options={{max:60}} >
+        <div className="Tilt-inner p3">
+            <img style={{paddingTop: "25%"}} src={Brain} alt="logo"></img>
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
