7eeeeed712f6bb234025f252e08ec5e7c15509e3
Nicolas Brandi <nmoyanobrandi@gmail.com>
2023-09-30 13:08:33 -0700

    create clarify api request setup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,58 @@ import Rank from './components/Rank/Rank';
 import ParticleBackground from './components/Particles/Particles';
 import './App.css';
 
+const setupClarifaiOptions = (imageURL) => {
+  const PAT = process.env.CLARIFAI_PAT;
+  const USER_ID = process.env.CLARIFAI_USER_ID;       
+  const APP_ID = process.env.CLARIFAI_APP_ID;
+  const IMAGE_URL = imageURL;
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  return {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+}
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onButtonClick = () => {
+    console.log('click')
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", setupClarifaiOptions)
+    .then(response => response.json())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  }
+
   render(){
     return (
       <div className="App">
@@ -14,7 +65,10 @@ class App extends Component {
         <Logo />
         <ParticleBackground />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonClick={this.onButtonClick}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
+require('dotenv').config();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
